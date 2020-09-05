05bd764a5776b5f944ed757ef064657085f73ab3
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-09-04 21:22:02 -0400

    Version 1

--- APISecureStudent/APISecureStudent/App_Start/IdentityConfig.cs
+++ APISecureStudent/APISecureStudent/App_Start/IdentityConfig.cs
@@ -29,7 +29,7 @@ namespace APISecureStudent
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
