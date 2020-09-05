0a9068e19cb803a4d9268acde3888d4a81e845df
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-09-05 10:29:07 -0400

    Se incluyo el proyecto UnitTest

---
+++ APISecureStudent/UTEstudianteSeguro.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTEstudianteSeguro.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTEstudianteSeguro.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7dcf1d96-7b80-4853-a979-2c40235fbc24")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
